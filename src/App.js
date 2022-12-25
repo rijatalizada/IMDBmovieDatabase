8dc2cbab3a9d9cb1b98f3fdf7449c26c902ec7d1
elo-319 <elshen.yusifov@bk.ru>
2022-12-26 01:30:17 +0400

    Pre Pull commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './Pages/Home'
+import Movies from './Pages/Movies';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/movies' element={<Movies/>} />
         </Routes>
       </Router>
     </div>
--- src/Pages/Movies.jsx
+++ src/Pages/Movies.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import useFetch from '../hooks/useFetch'
+
+const url = "http://localhost:3001/data"
 
 const Movies = () => {
+    const {data, loading, error} = useFetch(url)
+    console.log(data)
+
   return (
     <div>Movies</div>
   )
